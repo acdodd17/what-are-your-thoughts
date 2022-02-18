78cb121587260f7882a52b00e1b6a1cb16c6b25c
acdodd17 <acdodd17@gmail.com>
2022-02-18 14:09:29 -0500

    server connection

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-d
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.unsubscribe(express.json());
+app.use(express.urlencoded( { extended: true}));
+app.unsubscribe(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/what-are-your-thoughts', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌎 Connected on localhost: ${PORT}`));
