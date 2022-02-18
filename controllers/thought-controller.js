45e5e907b4b7a6297c2f62b89d654f954736d04a
acdodd17 <acdodd17@gmail.com>
2022-02-18 15:03:31 -0500

    controllers outline

---
+++ controllers/thought-controller.js
@@ -0,0 +1,19 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    // get all thoughts
+
+    // get a single thought by _id
+
+    // POST to create a new thought and push to user's thoughts array
+
+    // PUT to update a thought by _id
+
+    // DELETE to remove a thought by _id
+
+    // POST to create a reaction and push to single thought's reactions array
+
+    // DELETE to pull and remove a reaction by reactionId value
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-constroller.js
@@ -0,0 +1,20 @@
+const { User } = require('../models');
+
+const userController = { 
+    // get all users
+
+    // get a single user by _id and populate thought and friend data
+
+    // post a new user
+
+    // PUT to update user by _id
+
+    // DELETE to remove user by _id
+        // Remove User's associated thoughts when deleted
+
+    // POST to add a new friend to user's friend list
+
+    // DELETE to remove a friend from a user's friend list
+};
+
+module.exports = userController;
