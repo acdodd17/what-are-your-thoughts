96d789776449e1c21a636c888284f38dd4072d05
acdodd17 <acdodd17@gmail.com>
2022-02-21 10:00:51 -0500

    fixing server error

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.unsubscribe(express.json());
+app.use(express.json());
 app.use(express.urlencoded( { extended: true}));
-app.unsubscribe(express.static('public'));
+app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/what-are-your-thoughts', {
     useNewUrlParser: true, 
